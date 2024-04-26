8aa7d1f35555051d74d7b3591910513ee46844fe
sebmat003 <sebmat003@utp.edu.pl>
2024-04-26 12:46:11 +0200

    auth guard

--- 3.10-guards-completed/myBankApp/src/app/pages/login/login.component.ts
+++ 3.10-guards-completed/myBankApp/src/app/pages/login/login.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../shared/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  private readonly authService = inject(AuthService);
+  private router = inject(Router);
 
+  login() {
+    this.authService.login();
+    this.router.navigateByUrl('');
+  }
 }
--- 3.10-guards-starter/README.md
+++ 3.10-guards-starter/README.md
@@ -1,3 +1,8 @@
 # Tips
 
-- 
+- Add auth guard
+- Add auth service with a loggedIn boolean property, and methods login, logout (using signals)
+- In guard, implement canActivateFn to determine if user is logged in or not
+- Add button login to login page, and logout to dashboard
+- Timer should execute logout method from authService
+- Add guard to app.routes.ts
